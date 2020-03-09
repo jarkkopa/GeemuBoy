f4b194d34b773166751586c6ed28cfc94d0737ab
Jarkko Palokangas <hpmandito@gmail.com>
2020-03-09 18:47:23 +0200

    Move cpu load instructions to own LoadUnit to mitigate bloating CPU class and to separate instruction mapping and opcode logic in unit testing.

--- GameBoy/GB/CPU.cs
+++ GameBoy/GB/CPU.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using GameBoy.GB.CpuUnits;
+using System;
 using System.Collections.Generic;
 
 namespace GameBoy.GB
 {
     public class CPU
     {
+        private readonly LoadUnit loadUnit;
+
         public byte A;
         public byte B;
         public byte C;
@@ -14,19 +17,22 @@ namespace GameBoy.GB
         public byte H;
         public byte L;
 
-        public ushort PC { get; private set; }
+        public ushort PC;
+
         public ushort SP { get; private set; }
 
         public int Cycles { get; private set; }
 
         public Memory Memory { get; private set; }
 
-        private readonly Dictionary<byte, Action> _opCodes = new Dictionary<byte, Action>();
+        private readonly Dictionary<byte, Func<int>> _opCodes = new Dictionary<byte, Func<int>>();
         private readonly Dictionary<byte, string> _opCodeNames = new Dictionary<byte, string>();
 
 
         public CPU(Memory memory)
         {
+            loadUnit = new LoadUnit(memory);
+
             CreateOpCodes();
 
             this.Memory = memory;
@@ -53,7 +59,7 @@ namespace GameBoy.GB
             PC++;
             if (_opCodes.ContainsKey(code))
             {
-                _opCodes[code]();
+                Cycles = _opCodes[code]();
             }
             else
             {
@@ -67,160 +73,106 @@ namespace GameBoy.GB
             {
                 return _opCodeNames[code];
             }
-            return "";
+            return $"Unknown opcode: {code:x2}";
         }
 
         private void CreateOpCodes()
         {
             // LD nn, n
-            CreateOpCode(0x06, () => LoadImmediate8(ref B), "LD B, n");
-            CreateOpCode(0x0E, () => LoadImmediate8(ref C), "LD C, n");
-            CreateOpCode(0x16, () => LoadImmediate8(ref D), "LD D, n");
-            CreateOpCode(0x1E, () => LoadImmediate8(ref E), "LD E, n");
-            CreateOpCode(0x26, () => LoadImmediate8(ref H), "LD H, n");
-            CreateOpCode(0x2E, () => LoadImmediate8(ref L), "LD L, n");
+            CreateOpCode(0x06, () => loadUnit.LoadImmediate8(ref B, ref PC), "LD B, n");
+            CreateOpCode(0x0E, () => loadUnit.LoadImmediate8(ref C, ref PC), "LD C, n");
+            CreateOpCode(0x16, () => loadUnit.LoadImmediate8(ref D, ref PC), "LD D, n");
+            CreateOpCode(0x1E, () => loadUnit.LoadImmediate8(ref E, ref PC), "LD E, n");
+            CreateOpCode(0x26, () => loadUnit.LoadImmediate8(ref H, ref PC), "LD H, n");
+            CreateOpCode(0x2E, () => loadUnit.LoadImmediate8(ref L, ref PC), "LD L, n");
 
             //LD r1, r2
-            CreateOpCode(0x7F, () => Copy(ref A, ref A), "LD A, A");
-            CreateOpCode(0x78, () => Copy(ref A, ref B), "LD A, B");
-            CreateOpCode(0x79, () => Copy(ref A, ref C), "LD A, C");
-            CreateOpCode(0x7A, () => Copy(ref A, ref D), "LD A, D");
-            CreateOpCode(0x7B, () => Copy(ref A, ref E), "LD A, E");
-            CreateOpCode(0x7C, () => Copy(ref A, ref H), "LD A, H");
-            CreateOpCode(0x7D, () => Copy(ref A, ref L), "LD A, L");
-            CreateOpCode(0x7E, () => LoadFromAddress(ref A, BitUtils.BytesToUshort(H, L)), "LD A, (HL)");
+            CreateOpCode(0x7F, () => loadUnit.Copy(ref A, ref A), "LD A, A");
+            CreateOpCode(0x78, () => loadUnit.Copy(ref A, ref B), "LD A, B");
+            CreateOpCode(0x79, () => loadUnit.Copy(ref A, ref C), "LD A, C");
+            CreateOpCode(0x7A, () => loadUnit.Copy(ref A, ref D), "LD A, D");
+            CreateOpCode(0x7B, () => loadUnit.Copy(ref A, ref E), "LD A, E");
+            CreateOpCode(0x7C, () => loadUnit.Copy(ref A, ref H), "LD A, H");
+            CreateOpCode(0x7D, () => loadUnit.Copy(ref A, ref L), "LD A, L");
+            CreateOpCode(0x7E, () => loadUnit.LoadFromAddress(ref A, BitUtils.BytesToUshort(H, L)), "LD A, (HL)");
 
-            CreateOpCode(0x40, () => Copy(ref B, ref B), "LD B, B");
-            CreateOpCode(0x41, () => Copy(ref B, ref C), "LD B, C");
-            CreateOpCode(0x42, () => Copy(ref B, ref D), "LD B, D");
-            CreateOpCode(0x43, () => Copy(ref B, ref E), "LD B, E");
-            CreateOpCode(0x44, () => Copy(ref B, ref H), "LD B, H");
-            CreateOpCode(0x45, () => Copy(ref B, ref L), "LD B, L");
-            CreateOpCode(0x46, () => LoadFromAddress(ref B, BitUtils.BytesToUshort(H, L)), "LD B, (HL)");
+            CreateOpCode(0x40, () => loadUnit.Copy(ref B, ref B), "LD B, B");
+            CreateOpCode(0x41, () => loadUnit.Copy(ref B, ref C), "LD B, C");
+            CreateOpCode(0x42, () => loadUnit.Copy(ref B, ref D), "LD B, D");
+            CreateOpCode(0x43, () => loadUnit.Copy(ref B, ref E), "LD B, E");
+            CreateOpCode(0x44, () => loadUnit.Copy(ref B, ref H), "LD B, H");
+            CreateOpCode(0x45, () => loadUnit.Copy(ref B, ref L), "LD B, L");
+            CreateOpCode(0x46, () => loadUnit.LoadFromAddress(ref B, BitUtils.BytesToUshort(H, L)), "LD B, (HL)");
 
-            CreateOpCode(0x48, () => Copy(ref C, ref B), "LD C, B");
-            CreateOpCode(0x49, () => Copy(ref C, ref C), "LD C, C");
-            CreateOpCode(0x4A, () => Copy(ref C, ref D), "LD C, D");
-            CreateOpCode(0x4B, () => Copy(ref C, ref E), "LD C, E");
-            CreateOpCode(0x4C, () => Copy(ref C, ref H), "LD C, H");
-            CreateOpCode(0x4D, () => Copy(ref C, ref L), "LD C, L");
-            CreateOpCode(0x4E, () => LoadFromAddress(ref C, BitUtils.BytesToUshort(H, L)), "LD C, (HL)");
+            CreateOpCode(0x48, () => loadUnit.Copy(ref C, ref B), "LD C, B");
+            CreateOpCode(0x49, () => loadUnit.Copy(ref C, ref C), "LD C, C");
+            CreateOpCode(0x4A, () => loadUnit.Copy(ref C, ref D), "LD C, D");
+            CreateOpCode(0x4B, () => loadUnit.Copy(ref C, ref E), "LD C, E");
+            CreateOpCode(0x4C, () => loadUnit.Copy(ref C, ref H), "LD C, H");
+            CreateOpCode(0x4D, () => loadUnit.Copy(ref C, ref L), "LD C, L");
+            CreateOpCode(0x4E, () => loadUnit.LoadFromAddress(ref C, BitUtils.BytesToUshort(H, L)), "LD C, (HL)");
 
-            CreateOpCode(0x50, () => Copy(ref D, ref B), "LD D, B");
-            CreateOpCode(0x51, () => Copy(ref D, ref C), "LD D, C");
-            CreateOpCode(0x52, () => Copy(ref D, ref D), "LD D, D");
-            CreateOpCode(0x53, () => Copy(ref D, ref E), "LD D, E");
-            CreateOpCode(0x54, () => Copy(ref D, ref H), "LD D, H");
-            CreateOpCode(0x55, () => Copy(ref D, ref L), "LD D, L");
-            CreateOpCode(0x56, () => LoadFromAddress(ref D, BitUtils.BytesToUshort(H, L)), "LD D, (HL)");
+            CreateOpCode(0x50, () => loadUnit.Copy(ref D, ref B), "LD D, B");
+            CreateOpCode(0x51, () => loadUnit.Copy(ref D, ref C), "LD D, C");
+            CreateOpCode(0x52, () => loadUnit.Copy(ref D, ref D), "LD D, D");
+            CreateOpCode(0x53, () => loadUnit.Copy(ref D, ref E), "LD D, E");
+            CreateOpCode(0x54, () => loadUnit.Copy(ref D, ref H), "LD D, H");
+            CreateOpCode(0x55, () => loadUnit.Copy(ref D, ref L), "LD D, L");
+            CreateOpCode(0x56, () => loadUnit.LoadFromAddress(ref D, BitUtils.BytesToUshort(H, L)), "LD D, (HL)");
 
-            CreateOpCode(0x58, () => Copy(ref E, ref B), "LD E, B");
-            CreateOpCode(0x59, () => Copy(ref E, ref C), "LD E, C");
-            CreateOpCode(0x5A, () => Copy(ref E, ref D), "LD E, D");
-            CreateOpCode(0x5B, () => Copy(ref E, ref E), "LD E, E");
-            CreateOpCode(0x5C, () => Copy(ref E, ref H), "LD E, H");
-            CreateOpCode(0x5D, () => Copy(ref E, ref L), "LD E, L");
-            CreateOpCode(0x5E, () => LoadFromAddress(ref E, BitUtils.BytesToUshort(H, L)), "LD E, (HL)");
+            CreateOpCode(0x58, () => loadUnit.Copy(ref E, ref B), "LD E, B");
+            CreateOpCode(0x59, () => loadUnit.Copy(ref E, ref C), "LD E, C");
+            CreateOpCode(0x5A, () => loadUnit.Copy(ref E, ref D), "LD E, D");
+            CreateOpCode(0x5B, () => loadUnit.Copy(ref E, ref E), "LD E, E");
+            CreateOpCode(0x5C, () => loadUnit.Copy(ref E, ref H), "LD E, H");
+            CreateOpCode(0x5D, () => loadUnit.Copy(ref E, ref L), "LD E, L");
+            CreateOpCode(0x5E, () => loadUnit.LoadFromAddress(ref E, BitUtils.BytesToUshort(H, L)), "LD E, (HL)");
 
-            CreateOpCode(0x60, () => Copy(ref H, ref B), "LD H, B");
-            CreateOpCode(0x61, () => Copy(ref H, ref C), "LD H, C");
-            CreateOpCode(0x62, () => Copy(ref H, ref D), "LD H, D");
-            CreateOpCode(0x63, () => Copy(ref H, ref E), "LD H, E");
-            CreateOpCode(0x64, () => Copy(ref H, ref H), "LD H, H");
-            CreateOpCode(0x65, () => Copy(ref H, ref L), "LD H, L");
-            CreateOpCode(0x66, () => LoadFromAddress(ref H, BitUtils.BytesToUshort(H, L)), "LD H, (HL)");
+            CreateOpCode(0x60, () => loadUnit.Copy(ref H, ref B), "LD H, B");
+            CreateOpCode(0x61, () => loadUnit.Copy(ref H, ref C), "LD H, C");
+            CreateOpCode(0x62, () => loadUnit.Copy(ref H, ref D), "LD H, D");
+            CreateOpCode(0x63, () => loadUnit.Copy(ref H, ref E), "LD H, E");
+            CreateOpCode(0x64, () => loadUnit.Copy(ref H, ref H), "LD H, H");
+            CreateOpCode(0x65, () => loadUnit.Copy(ref H, ref L), "LD H, L");
+            CreateOpCode(0x66, () => loadUnit.LoadFromAddress(ref H, BitUtils.BytesToUshort(H, L)), "LD H, (HL)");
 
-            CreateOpCode(0x68, () => Copy(ref L, ref B), "LD L, B");
-            CreateOpCode(0x69, () => Copy(ref L, ref C), "LD L, C");
-            CreateOpCode(0x6A, () => Copy(ref L, ref D), "LD L, D");
-            CreateOpCode(0x6B, () => Copy(ref L, ref E), "LD L, E");
-            CreateOpCode(0x6C, () => Copy(ref L, ref H), "LD L, H");
-            CreateOpCode(0x6D, () => Copy(ref L, ref L), "LD L, L");
-            CreateOpCode(0x6E, () => LoadFromAddress(ref L, BitUtils.BytesToUshort(H, L)), "LD L, (HL)");
+            CreateOpCode(0x68, () => loadUnit.Copy(ref L, ref B), "LD L, B");
+            CreateOpCode(0x69, () => loadUnit.Copy(ref L, ref C), "LD L, C");
+            CreateOpCode(0x6A, () => loadUnit.Copy(ref L, ref D), "LD L, D");
+            CreateOpCode(0x6B, () => loadUnit.Copy(ref L, ref E), "LD L, E");
+            CreateOpCode(0x6C, () => loadUnit.Copy(ref L, ref H), "LD L, H");
+            CreateOpCode(0x6D, () => loadUnit.Copy(ref L, ref L), "LD L, L");
+            CreateOpCode(0x6E, () => loadUnit.LoadFromAddress(ref L, BitUtils.BytesToUshort(H, L)), "LD L, (HL)");
 
-            CreateOpCode(0x70, () => WriteToAddress(BitUtils.BytesToUshort(H, L), ref B), "LD (HL), B");
-            CreateOpCode(0x71, () => WriteToAddress(BitUtils.BytesToUshort(H, L), ref C), "LD (HL), C");
-            CreateOpCode(0x72, () => WriteToAddress(BitUtils.BytesToUshort(H, L), ref D), "LD (HL), D");
-            CreateOpCode(0x73, () => WriteToAddress(BitUtils.BytesToUshort(H, L), ref E), "LD (HL), E");
-            CreateOpCode(0x74, () => WriteToAddress(BitUtils.BytesToUshort(H, L), ref H), "LD (HL), H");
-            CreateOpCode(0x75, () => WriteToAddress(BitUtils.BytesToUshort(H, L), ref L), "LD (HL), L");
-            CreateOpCode(0x36, () => LoadImmediate8ToAddress(BitUtils.BytesToUshort(H, L)), "LD (HL), n");
+            CreateOpCode(0x70, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(H, L), ref B), "LD (HL), B");
+            CreateOpCode(0x71, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(H, L), ref C), "LD (HL), C");
+            CreateOpCode(0x72, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(H, L), ref D), "LD (HL), D");
+            CreateOpCode(0x73, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(H, L), ref E), "LD (HL), E");
+            CreateOpCode(0x74, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(H, L), ref H), "LD (HL), H");
+            CreateOpCode(0x75, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(H, L), ref L), "LD (HL), L");
+            CreateOpCode(0x36, () => loadUnit.LoadImmediate8ToAddress(BitUtils.BytesToUshort(H, L), ref PC), "LD (HL), n");
 
-            CreateOpCode(0x0A, () => LoadFromAddress(ref A, BitUtils.BytesToUshort(B, C)), "LD A, (BC)");
-            CreateOpCode(0x1A, () => LoadFromAddress(ref A, BitUtils.BytesToUshort(D, E)), "LD A, (DE)");
-            CreateOpCode(0xFA, () => LoadFromImmediateAddress(ref A), "LD A, (nn)");
-            CreateOpCode(0x3E, () => LoadImmediate8(ref A), "LD A, n");
+            CreateOpCode(0x0A, () => loadUnit.LoadFromAddress(ref A, BitUtils.BytesToUshort(B, C)), "LD A, (BC)");
+            CreateOpCode(0x1A, () => loadUnit.LoadFromAddress(ref A, BitUtils.BytesToUshort(D, E)), "LD A, (DE)");
+            CreateOpCode(0xFA, () => loadUnit.LoadFromImmediateAddress(ref A, ref PC), "LD A, (nn)");
+            CreateOpCode(0x3E, () => loadUnit.LoadImmediate8(ref A, ref PC), "LD A, n");
 
-            CreateOpCode(0x47, () => Copy(ref B, ref A), "LD B, A");
-            CreateOpCode(0x4F, () => Copy(ref C, ref A), "LD C, A");
-            CreateOpCode(0x57, () => Copy(ref D, ref A), "LD D, A");
-            CreateOpCode(0x5F, () => Copy(ref E, ref A), "LD E, A");
-            CreateOpCode(0x67, () => Copy(ref H, ref A), "LD H, A");
-            CreateOpCode(0x6F, () => Copy(ref L, ref A), "LD L, A");
-            CreateOpCode(0x02, () => WriteToAddress(BitUtils.BytesToUshort(B, C), ref A), "LD (BC), A");
-            CreateOpCode(0x12, () => WriteToAddress(BitUtils.BytesToUshort(D, E), ref A), "LD (DE), A");
-            CreateOpCode(0x77, () => WriteToAddress(BitUtils.BytesToUshort(H, L), ref A), "LD (HL), A");
-            CreateOpCode(0xEA, () => WriteImmediateAddress(ref A), "LD (nn), A");
+            CreateOpCode(0x47, () => loadUnit.Copy(ref B, ref A), "LD B, A");
+            CreateOpCode(0x4F, () => loadUnit.Copy(ref C, ref A), "LD C, A");
+            CreateOpCode(0x57, () => loadUnit.Copy(ref D, ref A), "LD D, A");
+            CreateOpCode(0x5F, () => loadUnit.Copy(ref E, ref A), "LD E, A");
+            CreateOpCode(0x67, () => loadUnit.Copy(ref H, ref A), "LD H, A");
+            CreateOpCode(0x6F, () => loadUnit.Copy(ref L, ref A), "LD L, A");
+            CreateOpCode(0x02, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(B, C), ref A), "LD (BC), A");
+            CreateOpCode(0x12, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(D, E), ref A), "LD (DE), A");
+            CreateOpCode(0x77, () => loadUnit.WriteToAddress(BitUtils.BytesToUshort(H, L), ref A), "LD (HL), A");
+            CreateOpCode(0xEA, () => loadUnit.WriteImmediateAddress(ref A, ref PC), "LD (nn), A");
         }
 
-        private void CreateOpCode(byte command, Action action, string name)
+        private void CreateOpCode(byte command, Func<int> action, string name)
         {
             _opCodes.Add(command, action);
             _opCodeNames.Add(command, name);
-        }
-
-        public void LoadImmediate8(ref byte dest)
-        {
-            dest = Memory.ReadByte(PC);
-            PC++;
-            Cycles = 8;
-        }
-
-        public void Copy(ref byte dest, ref byte source)
-        {
-            dest = source;
-            Cycles = 4;
-        }
-
-        public void LoadFromAddress(ref byte dest, ushort address)
-        {
-            byte value = Memory.ReadByte(address);
-            dest = value;
-            Cycles = 8;
-        }
-
-        public void WriteToAddress(ushort address, ref byte source)
-        {
-            Memory.WriteByte(address, source);
-            Cycles = 8;
-        }
-
-        public void WriteImmediateAddress(ref byte source)
-        {
-            byte addrHigh = Memory.ReadByte(PC);
-            PC++;
-            byte addrLow = Memory.ReadByte(PC);
-            PC++;
-            Memory.WriteByte(BitUtils.BytesToUshort(addrHigh, addrLow), source);
-            Cycles = 16;
-        }
-
-        public void LoadImmediate8ToAddress(ushort address)
-        {
-            byte data = Memory.ReadByte(PC);
-            PC++;
-            Memory.WriteByte(address, data);
-            Cycles = 12;
-        }
-
-        public void LoadFromImmediateAddress(ref byte dest)
-        {
-            byte addressHigh = Memory.ReadByte(PC);
-            PC++;
-            byte addressLow = Memory.ReadByte(PC);
-            PC++;
-            dest = Memory.ReadByte(BitUtils.BytesToUshort(addressHigh, addressLow));
-            Cycles = 16;
         }
     }
 }
---
+++ GameBoy/GB/CpuUnits/LoadUnit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameBoy.GB.CpuUnits
+{
+    public class LoadUnit
+    {
+        private Memory _memory;
+
+        public LoadUnit(Memory memory)
+        {
+            _memory = memory;
+        }
+
+        public int Copy(ref byte dest, ref byte source)
+        {
+            dest = source;
+            return 4;
+        }
+
+        public int LoadImmediate8(ref byte dest, ref ushort PC)
+        {
+            dest = _memory.ReadByte(PC);
+            PC++;
+            return 8;
+        }
+
+        public int LoadFromAddress(ref byte dest, ushort address)
+        {
+            byte value = _memory.ReadByte(address);
+            dest = value;
+            return 8;
+        }
+
+        public int WriteToAddress(ushort address, ref byte source)
+        {
+            _memory.WriteByte(address, source);
+            return 8;
+        }
+
+        public int WriteImmediateAddress(ref byte source, ref ushort PC)
+        {
+            byte addrHigh = _memory.ReadByte(PC);
+            PC++;
+            byte addrLow = _memory.ReadByte(PC);
+            PC++;
+            _memory.WriteByte(BitUtils.BytesToUshort(addrHigh, addrLow), source);
+            return 16;
+        }
+
+        public int LoadImmediate8ToAddress(ushort address, ref ushort PC)
+        {
+            byte data = _memory.ReadByte(PC);
+            PC++;
+            _memory.WriteByte(address, data);
+            return 12;
+        }
+
+        public int LoadFromImmediateAddress(ref byte dest, ref ushort PC)
+        {
+            byte addressHigh = _memory.ReadByte(PC);
+            PC++;
+            byte addressLow = _memory.ReadByte(PC);
+            PC++;
+            dest = _memory.ReadByte(BitUtils.BytesToUshort(addressHigh, addressLow));
+            return 16;
+        }
+    }
+}
