4456e94c895c94df98db16e774a00cc0b0131a09
Jarkko Palokangas <hpmandito@gmail.com>
2020-04-17 20:29:18 +0300

    Still renaming

--- GeemuBoy/Program.cs
+++ GeemuBoy/Program.cs
@@ -2,17 +2,13 @@
 {
     class Program
     {
-        private static readonly string romFolderPath = "C:/repos/GameBoy/GeemuBoy/Roms";
+        private static readonly string romFolderPath = "C:/repos/GeemuBoy/GeemuBoy/Roms";
 
         static void Main(string[] args)
         {
-            //string cartridgePath = "Roms/cpu_instrs.gb";
-            //string cartridgePath = $"{romFolderPath}/06-ld r,r.gb";
-            string cartridgePath = $"{romFolderPath}/Tetris.gb";
+            string cartridgePath = $"{romFolderPath}/06-ld r,r.gb";
             //string cartridgePath = "Roms/01-special.gb";
-            string bootRomPath = $"{romFolderPath }/DMG_ROM.bin";
-            //new Emulator(cartridgePath, bootRomPath);
-            new EmulatorSDL(cartridgePath, bootRomPath);
+            new EmulatorSDL(cartridgePath, null);
         }
     }
 }
